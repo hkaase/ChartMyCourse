d3f615776d4aaa8691e144305d977f7f42dfdbdf
hkaase <livin2chill@yahoo.com>
2022-04-04 12:56:53 -0500

    Rename classes to match naming conventions, more comments
    The effects of the gitignore ignoring class files are also taking place.

---
+++ ChartMyCourse/src/main/java/chartmycourse/chartmycourse/Post.java
@@ -0,0 +1,41 @@
+package chartmycourse.chartmycourse;
+
+public class Post {
+	String author, postContents;
+	
+	int replies, upvotes;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPostContents() {
+		return postContents;
+	}
+
+	public void setPostContents(String postContents) {
+		this.postContents = postContents;
+	}
+
+	public int getReplies() {
+		return replies;
+	}
+
+	public void setReplies(int replies) {
+		this.replies = replies;
+	}
+
+	public int getUpvotes() {
+		return upvotes;
+	}
+
+	public void setUpvotes(int upvotes) {
+		this.upvotes = upvotes;
+	}
+	
+	
+}
---
+++ ChartMyCourse/src/main/java/chartmycourse/chartmycourse/Review.java
@@ -0,0 +1,68 @@
+package chartmycourse.chartmycourse;
+
+import java.util.Objects;
+
+
+public class Review {
+    public String author;
+    public String CRN;
+    public String course;
+    public String professor;
+    int rating;
+    String reviewBody;
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCRN() {
+		return CRN;
+	}
+	public void setCRN(String cRN) {
+		CRN = cRN;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getProfessor() {
+		return professor;
+	}
+	public void setProfessor(String professor) {
+		this.professor = professor;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getReviewBody() {
+		return reviewBody;
+	}
+	public void setReviewBody(String reviewBody) {
+		this.reviewBody = reviewBody;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(CRN, author, course, professor, rating, reviewBody);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Review other = (Review) obj;
+		return Objects.equals(CRN, other.CRN) && Objects.equals(author, other.author)
+				&& Objects.equals(course, other.course) && Objects.equals(professor, other.professor)
+				&& rating == other.rating && Objects.equals(reviewBody, other.reviewBody);
+	}
+    
+    
+}
